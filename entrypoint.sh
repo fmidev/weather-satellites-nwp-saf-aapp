7acc2b76f2652f1a3a5aa64dd28cf36ccc2123d6
Panu Lahtinen <panu.lahtinen@fmi.fi>
2024-05-24 15:30:06 +0300

    Add a template for running AAPP

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+source /opt/AAPP_8.12/ATOVS_ENV8
+source /opt/conda/.bashrc
+# source /config/env-variables
+
+# /opt/conda/bin/aapp_dr_runner.py -v -c /config/aapp.yaml -e oper -s ${STATION} -n ${PUBLISH_NAMESERVER} -p ${PUBLISH_PORT}
+
+sleep 600
